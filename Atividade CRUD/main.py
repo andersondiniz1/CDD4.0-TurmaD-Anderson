2f2ecd07a3cb5bd28bc1af0559a02f5de5f68600
Anderson Celso <64502987+andersondiniz1@users.noreply.github.com>
2023-11-16 20:36:48 +0000

    crud3

--- Atividade CRUD/main.py
+++ Atividade CRUD/main.py
@@ -16,8 +16,35 @@ while True:
 
         # criar conta na academia
         if escolha == "1":
-            inserir_aluno()
-            print("\nConta criada com sucesso!")
+
+            print("=============================\n"
+                "Bem vindo ao cadastro academia CDD:\n"
+                "=============================\n"
+                "1. Cadastro alunos\n"
+                "2. Cadastro modalidade\n"
+                "3. Cadastro funcionarios\n"
+                "4. Cadastro Personal\n"
+                "0. Voltar ao menu principal\n"
+                "=============================\n")
+            
+            # repetição se escolher opção errado
+            escolha = input("Opção: ")
+            while escolha not in ["1", "2", "3", "4", "0"]:
+                escolha = input("\nOpção invalida, tente novamente: ")
+
+            if escolha == "1":
+                inserir_aluno()
+                print("\nConta aluno criada com sucesso!")
+
+            if escolha == "2":
+                print("\nConta aluno criada com sucesso!")
+            elif escolha == "3":
+                print("\nConta aluno criada com sucesso!")
+            elif escolha == "4":
+                print("\nConta aluno criada com sucesso!")
+            elif escolha == "0":
+                print("\nVoltando para o menu!")
+                break
 
         # verificar contas existentes
         elif escolha == "2":
